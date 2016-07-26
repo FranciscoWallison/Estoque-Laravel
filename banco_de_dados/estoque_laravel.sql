329e7af227fb8ede68c173b233dc33775daa419c
Wallison <franciscowallison@gmail.com>
2016-07-25 22:02:39 -0300

    Iniciando o Add dos banco e tabelas

---
+++ banco_de_dados/estoque_laravel.sql
@@ -0,0 +1,170 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: 26-Jul-2016 às 01:00
+-- Versão do servidor: 5.7.11
+-- PHP Version: 5.6.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `estoque_laravel`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `back_produtos`
+--
+
+CREATE TABLE `back_produtos` (
+  `ID` int(11) NOT NULL DEFAULT '0',
+  `nome` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `valor` varchar(255) CHARACTER SET latin1 DEFAULT NULL,
+  `descricao` varchar(255) CHARACTER SET latin1 DEFAULT NULL,
+  `quantidade` int(11) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `back_produtos`
+--
+
+INSERT INTO `back_produtos` (`ID`, `nome`, `valor`, `descricao`, `quantidade`) VALUES
+(1, 'Geladeira', '5900.00', 'Side by Side com gelo na porta', 2),
+(2, 'FogÃ£o', '950.00', 'Painel automÃ¡tico e forno elÃ©trico', 5),
+(3, 'Microondas', '1520.00', 'Manda SMS quando termina de esquentar', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `migration` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Extraindo dados da tabela `migrations`
+--
+
+INSERT INTO `migrations` (`migration`, `batch`) VALUES
+('2014_10_12_000000_create_users_table', 1),
+('2014_10_12_100000_create_password_resets_table', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `password_resets`
+--
+
+CREATE TABLE `password_resets` (
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `produtos`
+--
+
+CREATE TABLE `produtos` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `valor` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `descricao` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `quantidade` int(11) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Extraindo dados da tabela `produtos`
+--
+
+INSERT INTO `produtos` (`id`, `nome`, `valor`, `descricao`, `quantidade`) VALUES
+(1, 'Geladeira', '5900.00', 'Side by Side com gelo na porta', 2),
+(2, 'Fogão', '950.00', 'Painel automático e forno elétrico', 5),
+(3, 'Microondas', '1520.00', 'Manda SMS quando termina de esquentar', 1),
+(45, 'Café', 'asd', 'Java', 4),
+(46, 'Café', 'asd', 'Java', 4),
+(47, 'Café', 'asd', 'Java', 4),
+(48, 'Café', 'asd', 'Java', 4),
+(49, 'Café', 'asd', 'Java', 4),
+(50, 'Café', 'asd', 'Java', 4),
+(51, 'Arroz', '3', 'Branco', 1),
+(52, NULL, NULL, NULL, NULL),
+(53, 'Batata', '12', 'doce', 30),
+(54, 'Banana', '12', 'nanica', 4),
+(55, 'Batata', '12', 'Java', 43),
+(56, 'VideoGame', '4000', 'playstation', 4),
+(57, 'VideoGame', '4000', 'playstation', 4),
+(58, 'VideoGame', '4000', 'xbox 360', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(60) COLLATE utf8_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `password_resets`
+--
+ALTER TABLE `password_resets`
+  ADD KEY `password_resets_email_index` (`email`),
+  ADD KEY `password_resets_token_index` (`token`);
+
+--
+-- Indexes for table `produtos`
+--
+ALTER TABLE `produtos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_email_unique` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `produtos`
+--
+ALTER TABLE `produtos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=59;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
